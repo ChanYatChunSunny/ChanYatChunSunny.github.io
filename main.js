07a68d3e8bee7d91a51e5dd0bdbb44edbccf859c
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-02-14 16:35:03 +0800

    refactoring

--- main.js
+++ main.js
@@ -1,69 +1,64 @@
 /*Created by Chan Yat Chun, please give credits when using*/
 
-var btnClickCount = 0;
+let btnClickCount = 0;
 
 $(document).ready(function () {
-    switch (localStorage.getItem("loc")) {
-        case "summary":
-        case "skills":
-        case "programming_languages":
-        case "experiences":
-        case "other":
-        case "this":
-        case "contacts":
-        case "tools":
-            contentTyper(localStorage.getItem("loc"));
+    //This implementation of "page switching" is NOT great for SEO, this is merely a proof of concept 
+    //Ensure the value in local storage is valid
+    const possibleLocalItems = ["summary", "skills", "programming_languages", "experiences", "other", "this", "contacts", "tools"];
+    var storedItem = localStorage.getItem("loc");
+    var len = possibleLocalItems.length;
+    for (let i = 0; i < len; i++) {
+        if (storedItem === possibleLocalItems[i]) {
+            contentTyper(storedItem);
             break;
-        default:
-            localStorage.setItem("loc", "summary");
-            contentTyper(localStorage.getItem("loc"));
-            break;
+        }
     }
-    $('#summary-btn').click(function () {
+    //Assign functions to menu's btns
+    $("#summary-btn").click(function () {
         $(document).prop("title", "Sunny's summary");
         localStorage.setItem("loc", "summary");
         contentTyper("summary");
     });
-    $('#skills-btn').click(function () {
+    $("#skills-btn").click(function () {
         $(document).prop("title", "Sunny's skills");
         localStorage.setItem("loc", "skills");
         contentTyper("skills");
     });
-    $('#languages-btn').click(function () {
+    $("#languages-btn").click(function () {
         $(document).prop("title", "Sunny's programming languages");
         localStorage.setItem("loc", "programming_languages");
         contentTyper("programming_languages");
     });
-    $('#experiences-btn').click(function () {
+    $("#experiences-btn").click(function () {
         $(document).prop("title", "Sunny's experiences");
         localStorage.setItem("loc", "experiences");
         contentTyper("experiences");
     });
-    $('#other-btn').click(function () {
+    $("#other-btn").click(function () {
         $(document).prop("title", "Sunny's other trivials");
         localStorage.setItem("loc", "other");
         contentTyper("other");
     });
-    $('#this-btn').click(function () {
+    $("#this-btn").click(function () {
         $(document).prop("title", "Sunny's website");
         localStorage.setItem("loc", "this");
         contentTyper("this");
     });
-    $('#contacts-btn').click(function () {
+    $("#contacts-btn").click(function () {
         $(document).prop("title", "Sunny's contacts");
         localStorage.setItem("loc", "contacts");
         contentTyper("contacts");
     });
-    $('#tools-btn').click(function () {
-        $(document).prop("title", "Sunny's tools");
-        localStorage.setItem("loc", "tools");
-        contentTyper("tools");
+    $("#tools-btn").click(function () {
+        //Tools contains more features, so it will be better to be implemented in a separate page
+        window.location.href = "/tools";
     });
 });
 
 function contentTyper(contentName) {
     btnClickCount += 1;
-    var contentDisplay = $("#main-content");
+    let contentDisplay = $("#main-content");
     contentDisplay.html('');
     if (btnClickCount <= 1) {
         contentDisplay.fadeTo(0, 0);
