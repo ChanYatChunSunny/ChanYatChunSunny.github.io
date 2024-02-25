36b2da50c2fea5aab08d9d014b3ee7077e0152ad
ChanYatChunSunny <124800646+ChanYatChunSunny@users.noreply.github.com>
2024-02-25 14:57:35 +0800

    Added magic circle generator

---
+++ tools/magic_circle_generator.js
@@ -0,0 +1,146 @@
+$(document).ready(function () {
+    let magicCircleCanvas = $("#magic-circle-canvas")[0];
+    let canvasContext = magicCircleCanvas.getContext("2d");
+    let radiusSlider = $("#radius-slider");
+    let distanceSlider = $("#inner-outer-distance-slider");
+    let complexitySlider = $("#complexity-slider");
+
+    let radius = 192;
+    let distance = 32;
+    let complexity = 48;
+
+    radiusSlider.val(radius);
+    distanceSlider.val(distance);
+    complexitySlider.val(complexity);
+
+    radiusSlider.on("input", function () {
+        radius = $(this).val();
+    });
+    distanceSlider.on("input", function () {
+        distance = $(this).val();
+    });
+    complexitySlider.on("input", function () {
+        complexity = $(this).val();
+    });
+
+    $("#generate-btn").click(function () {
+        draw();
+    });
+
+    $("#download-btn").click(function () {
+        let temp = document.createElement("a");
+        temp.href = magicCircleCanvas.toDataURL("image/png");
+        temp.download = "magic_circle.png";
+        temp.click();
+    });
+
+    draw();
+
+    function draw() {
+        console.log(radius);
+        console.log(distance);
+        console.log(complexity);
+        let points = makeEdge();
+        let isBlobeds = makeBlobs(points);
+        connectPointsOnEdge(points, isBlobeds);
+    }
+
+    function makeEdge() {
+        canvasContext.fillStyle = "black";
+        canvasContext.fillRect(0, 0, magicCircleCanvas.width, magicCircleCanvas.height);
+        canvasContext.lineWidth = 4;
+        canvasContext.strokeStyle = "purple";
+        let originX = magicCircleCanvas.width / 2;
+        let originY = magicCircleCanvas.height / 2;
+        //Outer circle
+        canvasContext.beginPath();
+        canvasContext.arc(originX, originY, radius, 0, 2 * Math.PI);
+        canvasContext.stroke();
+        //Inner circle
+        let innerRadius = radius - distance;
+        canvasContext.beginPath();
+        canvasContext.arc(originX, originY, innerRadius, 0, 2 * Math.PI);
+        canvasContext.stroke();
+
+        let points = [];
+        let angleCounter = 0;
+
+        for (let i = 0; i < complexity; i++) {
+
+            let x = originX;
+            let y = originY;
+            if (Math.random() < 0.38) {
+                //Continuous angles
+                if (Math.random() < 0.9) {
+                    angleCounter += Math.random() / (0.2 * complexity);
+                    //Non-continuous angles
+                } else {
+                    angleCounter = Math.random();
+                }
+                let angle = angleCounter * 2 * Math.PI;
+                x += radius * Math.cos(angle);
+                y += radius * Math.sin(angle);
+            } else if (Math.random() < 0.96) {
+                //Continuous angles
+                if (Math.random() < 0.98) {
+                    angleCounter += Math.random() / (0.1 * complexity);
+                    //Non-continuous angles
+                } else {
+                    angleCounter = Math.random();
+                }
+                let angle = angleCounter * 2 * Math.PI;
+                x += innerRadius * Math.cos(angle);
+                y += innerRadius * Math.sin(angle);
+            }
+            points.push({ x: x, y: y });
+        }
+        return points
+    }
+
+    function makeBlobs(points) {
+        let isBlobeds = Array(complexity).fill(false);
+        canvasContext.lineWidth = 3;
+        canvasContext.strokeStyle = "purple";
+        for (let i = 0; i < complexity; i++) {
+            if (Math.random() < 0.32) {
+                canvasContext.beginPath();
+                canvasContext.arc(points[i].x, points[i].y, 8, 0, 2 * Math.PI);
+                canvasContext.stroke();
+                isBlobeds[i] = true;
+            }
+        }
+        return isBlobeds;
+    }
+
+    function connectPointsOnEdge(points, isBlobeds) {
+
+        canvasContext.lineWidth = 1;
+        canvasContext.strokeStyle = "purple";
+
+        //First stage connection
+        for (let i = 1; i < complexity; i++) {
+            if (isBlobeds[i] ? Math.random() < 0.8 : Math.random() < 0.6) {
+                canvasContext.beginPath();
+                canvasContext.moveTo(points[i - 1].x, points[i - 1].y);
+                canvasContext.lineTo(points[i].x, points[i].y);
+                canvasContext.stroke();
+            }
+        }
+
+        //Second stage connection
+        let halfComplexity = Math.floor(complexity / 2);
+        let maxRandConnectCount = Math.floor(halfComplexity + Math.random() * complexity);
+        for (let i = 0; i < maxRandConnectCount; i++) {
+            let a = Math.floor(Math.random() * complexity);
+            let b = Math.floor(Math.random() * complexity);
+            let aAcceptProb = isBlobeds[a] ? 0.8 : 0.4;
+            let bAcceptProb = isBlobeds[b] ? 0.8 : 0.4;
+            if (!(Math.random() < aAcceptProb * bAcceptProb)) { continue; }
+            canvasContext.beginPath();
+            canvasContext.moveTo(points[a].x, points[a].y);
+            canvasContext.lineTo(points[b].x, points[b].y);
+            canvasContext.stroke();
+        }  
+    }
+ 
+});
--- tools/tools.js
+++ tools/tools.js
@@ -9,4 +9,7 @@ $(document).ready(function () {
     $("#color-picker-btn").click(function () {
         window.location.href = "/tools/color_picker.html";
     });
+    $("#magic-circle-generator-btn").click(function () {
+        window.location.href = "/tools/magic_circle_generator.html";
+    });
 });
